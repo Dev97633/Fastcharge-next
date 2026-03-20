1fa2aca338c07b35f4affdec2663e6853c0a4811
DEV <devendar87502@gmail.com>
2026-03-20 15:40:09 +0530

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,19 +1,1 @@
-#!/bin/sh
 
-MODDIR="/data/adb/modules/fastcharge-next"
-LOG_FILE="$MODDIR/fastcharge.log"
-
-# Create module directory if missing
-mkdir -p "$MODDIR" 2>/dev/null
-
-# Create log file if missing
-if [ ! -f "$LOG_FILE" ]; then
-    : > "$LOG_FILE"
-    chmod 600 "$LOG_FILE"
-fi
-
-# Log startup
-ts=$(date +"%Y-%m-%d %H:%M:%S")
-printf "%s %s\n" "$ts" "post-fs-data: initialized" >> "$LOG_FILE"
-
-exit 0
